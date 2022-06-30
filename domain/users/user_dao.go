7e9f902ff5ae35a498b28f4b09049c2a82e7e43e
aryar <arya.rizky4@gmail.com>
2022-06-30 14:40:07 +0700

    fix

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -10,10 +10,10 @@ import (
 
 const (
 	queryInsertUser = "INSERT INTO users(first_name, last_name, email, date_created) VALUES(?,?,?,?);"
-	queryGetUser    = "SELECT id, first_name, last_name, email, date_created FROM users WHERE id=?"
+	queryGetUser    = "SELECT id, first_name, last_name, email, date_created FROM users WHERE id=?;"
 )
 
-func (user User) Get() *errors.RestErr {
+func (user *User) Get() *errors.RestErr {
 	stmt, err := users.Client.Prepare(queryGetUser)
 	if err != nil {
 		return errors.NewInternalServerError(err.Error())
@@ -23,10 +23,10 @@ func (user User) Get() *errors.RestErr {
 	result := stmt.QueryRow(user.Id)
 	if err := result.Scan(&user.Id, &user.FirstName, &user.LastName, &user.Email, &user.DateCreated); err != nil {
 		if strings.Contains(err.Error(), "no rows in result set") {
-			return errors.NewNotFoundError(fmt.Sprintf("user id %s not found", user.Id))
+			return errors.NewNotFoundError(fmt.Sprintf("user id %d not found", user.Id))
 		}
 		fmt.Println(err)
-		return errors.NewInternalServerError(fmt.Sprintf("error when trying to get user %s", user.Id))
+		return errors.NewInternalServerError(fmt.Sprintf("error when trying to get user %d", user.Id))
 	}
 
 	return nil
